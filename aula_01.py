5042da7facf03619b43143227113a091eabafa15
maezono00 <arthur.mmaezono00@gmail.com>
2026-07-08 14:53:39 -0300

    FEAT: Exercícios e anotações feitos durante o curso até agora.

---
+++ aula_01.py
@@ -0,0 +1,20 @@
+# O comando print exibe uma informação na tela do usuário.
+# print("Olá, mundo!!!")
+# print("Iniciando treinamento de Análise de Dados!!! 🥳🥳🥳")
+# print(30) #IDADE DO USUÁRIO
+# print("30") #Idade do Usuário
+
+idade = 30
+idade_dois = "30"
+
+# print(idade)
+# print(idade_dois)
+
+idade_futura = idade + 1
+# idade_futura_dois = idade_dois + 1
+
+print(idade_futura)
+# print(idade_futura_dois)
+
+print(type(idade))
+print(type(idade_dois))
